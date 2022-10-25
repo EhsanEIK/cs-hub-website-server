a2033e4e7ea55e15f0b8e6e6d72d8d7d1defa2cf
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-25 09:27:34 +0600

    addec cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 
 const courses = require('./data/courses.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('CS Server is running');
