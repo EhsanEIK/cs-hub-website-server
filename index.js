d46fd772880e147a4156ca1bd250f982ad84d102
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-25 09:26:35 +0600

    added courses route and send courses data in response

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('CS Server is running');
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
 })
 
 app.listen(port, () => {
