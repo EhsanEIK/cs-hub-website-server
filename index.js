45e091e1f4f6af26cfba2d17235016b1039bbd3e
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-25 10:45:08 +0600

    added single course api to get a single course from courses

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ const courses = require('./data/courses.json');
 
 app.use(cors());
 
+// default route
 app.get('/', (req, res) => {
     res.send('CS Server is running');
-})
+});
 
+// get all courses api
 app.get('/courses', (req, res) => {
     res.send(courses);
-})
+});
+
+// get a single course api
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(c => c.id === id);
+    res.send(course);
+});
 
 app.listen(port, () => {
     console.log('cs server is running on port:', port);
-})
+});
